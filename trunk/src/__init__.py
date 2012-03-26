c784aedfda1f21af0ef6323831908c316417636f
Andy Blackburn <andyblac@o2.co.uk>
2012-03-26 16:36:17 +0100

    remove some debug code.

--- trunk/src/__init__.py
+++ trunk/src/__init__.py
@@ -9,7 +9,7 @@ PluginLanguagePath = "SystemPlugins/ViX/po"
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
 	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-	print "[ViX] set language to ", lang
+# 	print "[ViX] set language to ", lang
 	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 	gettext.bindtextdomain('enigma2', resolveFilename(SCOPE_LANGUAGE, ""))
 
